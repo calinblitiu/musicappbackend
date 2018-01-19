bdc586d8a3a6117570a3e2db60f949cd6f8e61dd
stardev0220 <developer0220@outlook.com>
2018-01-20 04:52:55 +0800

    update sync4 backend

---
+++ WhoopTriggerz/application/controllers/images.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CStar
+ * Date: 1/20/2018
+ * Time: 4:38 AM
+ */
+
+class Images
+{
+
+}
---
+++ WhoopTriggerz/application/models/images_model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CStar
+ * Date: 1/20/2018
+ * Time: 4:41 AM
+ */
+
+class Images_model
+{
+
+}
